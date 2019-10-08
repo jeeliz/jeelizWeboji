4bfec602893189ee13695f617e97c157835795e0
Xavier Bourry <xavier.bourry@polytechnique.org>
2019-10-08 19:08:30 -0400

    [DEBUG] Cordova app - improve error reporting

--- demos/cordova/raccoon/www/js/index.js
+++ demos/cordova/raccoon/www/js/index.js
@@ -14,69 +14,79 @@ var jeefacetransferCanvas;
 var vidElement;
 
 function onDeviceReady() {
-    canvas = document.getElementById('canvas');
-    canvasWeb = document.getElementById('canvasWebgl');
-    jeefacetransferCanvas = document.getElementById('jeefacetransferCanvas');
-    //gl = canvasWeb.getContext("webgl"); jeefacetransferCanvas
-    //gl = canvasWeb.getContext("jeefacetransferCanvas");
+  canvas = document.getElementById('canvas');
+  canvasWeb = document.getElementById('canvasWebgl');
+  jeefacetransferCanvas = document.getElementById('jeefacetransferCanvas');
+  //gl = canvasWeb.getContext("webgl"); jeefacetransferCanvas
+  //gl = canvasWeb.getContext("jeefacetransferCanvas");
 
-    CanvasCamera.initialize(canvas);
+  CanvasCamera.initialize(canvas);
 
-    var options = {
-        cameraFacing: 'front',
-        fps:30,
-        width: 288,
-        height: 352,
-        canvas: {
-            width: 288,
-            height: 352
-        },
-        capture: {
-            width: 288,
-            height: 352
-        },
-        onAfterDraw: function(frame) {
-            //var dest;
-            //dest = canvasWeb.getContext('2d');
-            //dest.drawImage(canvas,0,0);
-        }
-    };
-    CanvasCamera.start(options);
+  var options = {
+    cameraFacing: 'front',
+    fps:30,
+    width: 288,
+    height: 352,
+    canvas: {
+      width: 288,
+      height: 352
+    },
+    capture: {
+      width: 288,
+      height: 352
+    },
+    onAfterDraw: function(frame) {
+      //var dest;
+      //dest = canvasWeb.getContext('2d');
+      //dest.drawImage(canvas,0,0);
+    }
+  };
+  CanvasCamera.start(options);
 
-    console.log("canv",canvas);
+  console.log("canv",canvas);
 
-    initWeboji();
+  initWeboji();
 }
 
 function initWeboji(){
-    alert("initialising jeeliz");    
-    console.log("using source image of ",canvas);
+  alert("initialising jeeliz");  
+  console.log("using source image of ",canvas);
 
-    THREE.JeelizHelper.onLoad=function(){
-        console.log("jeelize canvas is: ",THREE.JeelizHelper.get_cv() );
-    };
-    
+  /*THREE.JeelizHelper.onLoad = function(){
+  alert('JeelizHelper.onLoad called')
+  console.log("jeelize canvas is: ",THREE.JeelizHelper.get_cv() );
+  };*/
+  try (){
   THREE.JeelizHelper.init({
-    canvasThreeId: 'canvasWebgl',
-    canvasId: 'jeefacetransferCanvas',
+      canvasThreeId: 'canvasWebgl',
+      canvasId: 'jeefacetransferCanvas',
 
-    assetsParentPath: './assets/3D/',
-    NNCpath: './js/dist/',
+      assetsParentPath: './assets/3D/',
+      NNCpath: './js/dist/',
 
-     videoSettings: {
-      videoElement: canvas
-    },
+      videoSettings: {
+        videoElement: canvas
+      },
 
-    //FOX :
-    meshURL: 'meshes/fox11_v0.json',
-    matParameters: {
-      diffuseMapURL: 'textures/Fox_albedo.png',
-      specularMapURL: 'textures/Fox_specular.png',
-      flexMapURL: 'textures/Fox_flex.png'
-    },
-    position: [0,-80, 0],
-    scale: 1.2
+      //FOX :
+      meshURL: 'meshes/fox11_v0.json',
+      matParameters: {
+        diffuseMapURL: 'textures/Fox_albedo.png',
+        specularMapURL: 'textures/Fox_specular.png',
+        flexMapURL: 'textures/Fox_flex.png'
+      },
+      successCallback: function(){
+        alert('successCallback');
+      },
+      errorCallback: function(errCode){
+        alert('error: ' + errCode);
+      }
+      position: [0,-80, 0],
+      scale: 1.2
   });
+  } catch(e) {
+    alert('JS ERROR: ' + e.message);
+  } 
 } //end main()
 
 app.initialize();
